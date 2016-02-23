880ddb337d49d09bd940fd6bd7c030c1efee0995
xuqingfeng <js-xqf@hotmail.com>
2016-02-23 21:36:51 +0800

    revert

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
 package main
+
 import (
-	"net/http"
-	"flag"
-	"strconv"
-	"fmt"
-	"os"
+    "net/http"
+    "flag"
+    "strconv"
+    "fmt"
+    "os"
 )
 
 func main() {
 
-	var (
-		port *int = flag.Int(
-			"p", 8888, "port",
-		)
-		directory *string = flag.String(
-			"d", "./", "directory",
-		)
-	)
+    var (
+        port *int = flag.Int(
+            "p", 8888, "port",
+        )
+        directory *string = flag.String(
+            "d", "./", "directory",
+        )
+    )
 
-	flag.Parse()
+    flag.Parse()
 
-	fmt.Printf("Server running at localhost:%d on %s\n", *port, *directory)
-	fmt.Println("Ctrl-C to exit.")
+    fmt.Printf("Server running at localhost:%d on %s\n", *port, *directory)
+    fmt.Println("Ctrl-C to exit.")
 
-	err := http.ListenAndServe(":" + strconv.Itoa(*port), http.FileServer(http.Dir(*directory)))
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-		os.Exit(1)
-	}
+    err := http.ListenAndServe(":" + strconv.Itoa(*port), http.FileServer(http.Dir(*directory)))
+    if err != nil {
+        fmt.Printf("Error: %v", err)
+        os.Exit(1)
+    }
 }
