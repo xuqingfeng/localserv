8bd8f347b6f89048f4b7f0624e2f89f24be5998c
xuqingfeng <js-xqf@hotmail.com>
2016-02-23 21:34:55 +0800

    Revert "go build fail"
    This reverts commit 68c78604bbe70a41f3c6af740a674374c1c29e05.

--- main.go
+++ main.go
@@ -1,26 +1,20 @@
 package main
-
 import (
 	"net/http"
 	"flag"
 	"strconv"
 	"fmt"
 	"os"
-	"bitbucket.org/xuqingfeng/localserv/vendor/github.com/abbot/go-http-auth"
 )
 
 func main() {
 
 	var (
-		err error
 		port *int = flag.Int(
 			"p", 8888, "port",
 		)
 		directory *string = flag.String(
 			"d", "./", "directory",
-		)
-		basicAuth *bool = flag.Bool(
-			"a", false, "basic auth",
 		)
 	)
 
@@ -29,26 +23,9 @@ func main() {
 	fmt.Printf("Server running at localhost:%d on %s\n", *port, *directory)
 	fmt.Println("Ctrl-C to exit.")
 
-	if (!basicAuth) {
-		err = http.ListenAndServe(":" + strconv.Itoa(*port), http.FileServer(http.Dir(*directory)))
-		if err != nil {
-			fmt.Printf("Error: %v", err)
-			os.Exit(1)
-		}
-	}else {
-		authenticator := auth.NewBasicAuthenticator("localhost", secret)
-		http.HandleFunc("/", authenticator.Wrap(func() http.Handler {
-			return http.FileServer(http.Dir(*directory))
-		})())
-		err = http.ListenAndServe(":" + strconv.Itoa(*port), http.FileServer(http.Dir(*directory)))
+	err := http.ListenAndServe(":" + strconv.Itoa(*port), http.FileServer(http.Dir(*directory)))
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
 	}
-
-}
-
-func secret(user, realm string) string {
-
-	if ("xqf" == user) {
-		return "localserv"
-	}
-	return ""
 }
